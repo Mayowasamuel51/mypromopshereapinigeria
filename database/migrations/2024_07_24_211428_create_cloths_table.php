5c136640205f0a2bd3b4c56b5a34952942a60176
mayowasamuel <fpasamuelmayowa51@gmail.com>
2024-08-14 06:23:58 +0100

    DFdf

--- database/migrations/2024_07_24_211428_create_cloths_table.php
+++ database/migrations/2024_07_24_211428_create_cloths_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('cloths', function (Blueprint $table) {
-            $table->id();
+            $table->id();;
             $table->foreignIdFor(User::class)->nullable();
             $table->string("discount")->nullable();
             $table->string("user_image")->nullable();
